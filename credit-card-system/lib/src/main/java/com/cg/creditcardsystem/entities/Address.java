4cc8e72a34546eb454259a666c40c945d8b3bbbf
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2022-05-25 22:32:51 +0530

    from chetan

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/entities/Address.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/entities/Address.java
@@ -1,8 +1,43 @@
 package com.cg.creditcardsystem.entities;
 
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+
 public class Address {
-   private String city;
-   private String state;
-   private int pincode;
+	@JoinColumn
+	private UserRegistration userID;
+	@Column(name="city",length=30)
+    private String city;
+	@Column(name="state",length=30)
+    private String state;
+	@Column(name="pincode",length=6, nullable=false)
+    private int pincode;
+	
+	
+	public UserRegistration getUserID() {
+		return userID;
+	}
+	public void setUserID(UserRegistration userID) {
+		this.userID = userID;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
    
+	
 }
