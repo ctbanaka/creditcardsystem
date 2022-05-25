ea13f98d59a7d7bc54200541e394d07153ec4eb8
Babukalluru <kalluru.babu@capgemini.com>
2022-05-25 16:44:15 +0530

    Update UserRegistration.java

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/entities/UserRegistration.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/entities/UserRegistration.java
@@ -1,5 +1,23 @@
 package com.cg.creditcardsystem.entities;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="userregistration")
 public class UserRegistration {
-  private int userID;
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private int userID;
+    private String firstname;
+    private String lastname;
+    private Date dateofbirth;
+    private long phoneno;
+    private String email;
+    private String password;
 }
