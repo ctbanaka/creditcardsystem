0006ca497e21aef161c1bd7fe1a36176edf3a30f
Babukalluru <kalluru.babu@capgemini.com>
2022-05-27 11:47:50 +0530

    Update TransactionDto.java

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/dto/TransactionDto.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/dto/TransactionDto.java
@@ -11,5 +11,51 @@ public class TransactionDto {
     private double debitedBalance;
     private double availableBalance;
     private int redeemPoints;
-
+    
+	public long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public Date getTransactionDate() {
+		return transactionDate;
+	}
+	public void setTransactionDate(Date transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+	public String getTransactionType() {
+		return transactionType;
+	}
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+	public double getDebitedBalance() {
+		return debitedBalance;
+	}
+	public void setDebitedBalance(double debitedBalance) {
+		this.debitedBalance = debitedBalance;
+	}
+	public double getAvailableBalance() {
+		return availableBalance;
+	}
+	public void setAvailableBalance(double availableBalance) {
+		this.availableBalance = availableBalance;
+	}
+	public int getRedeemPoints() {
+		return redeemPoints;
+	}
+	public void setRedeemPoints(int redeemPoints) {
+		this.redeemPoints = redeemPoints;
+	}
+	@Override
+	public String toString() {
+		return "TransactionDto [transactionId=" + transactionId + ", transactionDate=" + transactionDate
+				+ ", transactionType=" + transactionType + ", debitedBalance=" + debitedBalance + ", availableBalance="
+				+ availableBalance + ", redeemPoints=" + redeemPoints + "]";
+	}
+	
 }
+	
+    
+
