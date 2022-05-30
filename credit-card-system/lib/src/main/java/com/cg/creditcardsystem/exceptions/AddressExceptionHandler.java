33ee8cd81106e3520b9e2f4f483788b857d737bc
saurabhsase <saurabh.sase@gmail.com>
2022-05-30 17:16:16 +0530

    updated

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/exceptions/AddressExceptionHandler.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/exceptions/AddressExceptionHandler.java
@@ -1,5 +1,15 @@
 package com.cg.creditcardsystem.exceptions;
 
-public class AddressExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
+public class AddressExceptionHandler {
+	@ExceptionHandler(value=AddressNotFoundExcetpion.class)
+  public ResponseEntity<Object> exception(AddressNotFoundExcetpion exception){
+	return new ResponseEntity<Object>("address not found..",HttpStatus.NOT_FOUND);
+	  
+  }
 }
