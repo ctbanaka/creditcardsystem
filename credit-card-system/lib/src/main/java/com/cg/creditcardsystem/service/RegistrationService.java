9dd7dc9462c63502749667154bc4a57628838bbd
HEMANTH510015 <HEMANTH.I@CAPGEMINI.COM>
2022-05-30 16:18:45 +0530

    update

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/service/RegistrationService.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/service/RegistrationService.java
@@ -5,12 +5,14 @@ import java.util.List;
 import java.util.Optional;
 
 import com.cg.creditcardsystem.entities.Registration;
+import com.cg.creditcardsystem.exceptions.UserAlreadyExistException;
+import com.cg.creditcardsystem.exceptions.UserNotFoundException;
 
 public interface RegistrationService {
-     void addUser(Registration reg);
+     void addUser(Registration reg) throws UserAlreadyExistException;
      List<Registration> viewAllUserDetails();
      void deleteUser(int userid);
      void updateUserDetails(Registration reg);
-     Optional<Registration> getRegById(int userid);
+     Optional<Registration> getRegById(int userid) throws UserNotFoundException;
 }
 
