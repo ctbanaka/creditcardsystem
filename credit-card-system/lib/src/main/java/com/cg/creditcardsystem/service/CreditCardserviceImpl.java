7d90a8badc3f24ebcc8e37a3bf61909fb35fdb38
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2022-05-30 15:15:49 +0530

    opo

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/service/CreditCardserviceImpl.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/service/CreditCardserviceImpl.java
@@ -8,50 +8,10 @@ import com.cg.creditcardsystem.entities.CreditCard;
 import com.cg.creditcardsystem.entities.Registration;
 import com.cg.creditcardsystem.repository.CreditCardRepository;
 import com.cg.creditcardsystem.repository.RegistrationRepository;
-import com.google.common.base.Optional;
 
-import antlr.collections.List;
 @Service
 public class CreditCardserviceImpl implements CreditCardService{
-	
-<<<<<<< HEAD
-//    @Autowired
-//	CreditCardRepository cardrepo;
-//    @Autowired
-//    RegistrationRepository regrepo;
-//	@Override
-//	public Optional<CreditCard> findAllActiveByUerId(int userid) {
-//		Optional<CreditCard> cred=cardrepo.findById(userid);
-//		return cred;
-//	
-//	}
-//	@Override
-//	public Optional<CreditCard> findById(int userid) {
-//		Optional<CreditCard> cred=cardrepo.findById(userid);
-//		return cred;
-//	}
-//	@Override
-//	public Optional<CreditCard> findActiveById(int userid) {
-//		Optional<CreditCard> cred=cardrepo.findById(userid);
-//		return cred;
-//	
-//	}
-//	@Override
-//	public boolean activate(int userid) {
-//		
-//		return false;
-//	}
-//	@Override
-//	public boolean deactivate(int userid) {
-//	
-//		return false;	
-//	}
-//	@Override
-//	public boolean changeDailyLimit(CreditCard card) {
-//		Optional<CreditCard>cred=cardrepo.ChangeByCardno(card);
-//		return false;
-//	}
-=======
+
     @Autowired
 	CreditCardRepository cardrepo;
     @Autowired
@@ -76,10 +36,4 @@ public class CreditCardserviceImpl implements CreditCardService{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	 
->>>>>>> edec6f5a6a12f441c01a81082ac2ce82454cbd95
 }
-//
-//
-//
-//	
