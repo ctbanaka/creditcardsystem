02f4a04d15621388ab46f7938105b183e1768f8e
Babukalluru <kalluru.babu@capgemini.com>
2022-05-31 18:48:11 +0530

    testcases

---
+++ credit-card-system/lib/src/test/java/com/cg/creditcardsystem/junittest/RegistrationControllerTest.java
@@ -0,0 +1,27 @@
+package com.cg.creditcardsystem.junittest;
+
+import org.junit.jupiter.api.Test;
+
+class RegistrationControllerTest {
+
+	@Test
+	void testUserRegistration() {
+		System.out.println("user registration is done");
+	}
+
+	@Test
+	void testGetAllRegistration() {
+		System.out.println("Succsessfully got All Registration");
+	}
+
+	@Test
+	void testDeleteRegistration() {
+		System.out.println("Delete registration is done");
+	}
+
+	@Test
+	void testEditRegistration() {
+		System.out.println("Edit registration is done");
+	}
+
+}
