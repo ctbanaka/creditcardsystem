aea286e10070deeab1e0d8262f38bf6c8461b766
saurabhsase <saurabh.sase@gmail.com>
2022-06-01 01:07:21 +0530

    updated

--- credit-card-system/lib/src/test/java/com/cg/creditcardsystem/test/AddressDaoTest.java
+++ credit-card-system/lib/src/test/java/com/cg/creditcardsystem/test/AddressDaoTest.java
@@ -1,6 +1,7 @@
 package com.cg.creditcardsystem.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 
@@ -8,8 +9,12 @@ import java.util.List;
  import org.springframework.beans.factory.annotation.Autowired;
  import org.springframework.boot.test.context.SpringBootTest;
 
- import com.cg.creditcardsystem.entities.Address;
+import com.cg.creditcardsystem.dto.AddressDto;
+import com.cg.creditcardsystem.entities.Address;
  import com.cg.creditcardsystem.repository.AddressRepository;
+import com.cg.creditcardsystem.repository.RegistrationRepository;
+import com.cg.creditcardsystem.service.AddressService;
+import com.cg.creditcardsystem.service.AddressServiceImpl;
 
 
 
@@ -18,16 +23,21 @@ import java.util.List;
 public class AddressDaoTest {
     @Autowired
     AddressRepository adrsrepo;
+   @Autowired
+   RegistrationRepository regrepo;
+   @Autowired
+   AddressService adrservice = new AddressServiceImpl();
     
-    @Test
-    public void testGetAddrById() {
-    	Address addr = adrsrepo.getAddress(30);
-    	System.out.println(addr.getAddressId());
-    	assertEquals(addr.getAddressId(),30);
-    }
-    
-   @Test
-    void testGetAllItemsNotNull() {
-    	List<Address> addrlist = adrsrepo.findAll();
-    }
+  @Test
+  void testAddAddress() {
+	  AddressDto adrsdto = new AddressDto();
+	  adrsdto.setCity("Aurangabd");
+      adrsdto.setState("Maharashtra");
+      adrsdto.setPinCode(431002);
+      adrsdto.setUserId(1000);
+      
+       int addressid = AddressService.addAddress(adrsdto);
+      Address adrs =adrsrepo.getById(addressid);
+      assertEquals(addressid, adrs.getAddressId());
+  }
 }
