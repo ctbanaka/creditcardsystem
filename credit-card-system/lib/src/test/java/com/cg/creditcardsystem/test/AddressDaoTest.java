dc4b6c6d35a40dc593300ae65ded4f4a0fd4eae2
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2022-05-31 17:01:14 +0530

    pusj

--- credit-card-system/lib/src/test/java/com/cg/creditcardsystem/test/AddressDaoTest.java
+++ credit-card-system/lib/src/test/java/com/cg/creditcardsystem/test/AddressDaoTest.java
@@ -1,34 +1,33 @@
 package com.cg.creditcardsystem.test;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//import java.util.List;
+import java.util.List;
 
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
+ import org.junit.jupiter.api.Test;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.boot.test.context.SpringBootTest;
 
-//import com.cg.creditcardsystem.entities.Address;
-// com.cg.deptemp.entities.Department;
-//import com.cg.deptemp.repository.DepartmentRepository;
+ import com.cg.creditcardsystem.entities.Address;
+ import com.cg.creditcardsystem.repository.AddressRepository;
 
 
-//@SpringBootTest
-//public class AddressDaoTest {
-    //@Autowired
-    //AddressRepository adrsrepo;
+
+
+@SpringBootTest
+public class AddressDaoTest {
+    @Autowired
+    AddressRepository adrsrepo;
     
-   // @Test
-    //public void testGetAddrById() {
-    	//Address addr = adrsrepo.getAddress(30);
-    	//System.out.println(addr.getAddressId());
-    	//assertEquals(addr.getAddressId(),30);
-    //}
+    @Test
+    public void testGetAddrById() {
+    	Address addr = adrsrepo.getAddress(30);
+    	System.out.println(addr.getAddressId());
+    	assertEquals(addr.getAddressId(),30);
+    }
     
-  //  @Test
-   // void testGetAllItemsNotNull() {
-    	//List<Address> addrlist = adrsrepo.findAll();
-    //}
-//}
+   @Test
+    void testGetAllItemsNotNull() {
+    	List<Address> addrlist = adrsrepo.findAll();
+    }
+}
