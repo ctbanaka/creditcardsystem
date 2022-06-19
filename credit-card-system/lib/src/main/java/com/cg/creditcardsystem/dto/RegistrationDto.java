4309990e51f658cd54e4893e0b307f6b1cc18696
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2022-06-19 10:11:34 +0530

    dto

---
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/dto/RegistrationDto.java
@@ -0,0 +1,51 @@
+package com.cg.creditcardsystem.dto;
+
+import java.util.Date;
+
+public class RegistrationDto {
+	   private int userId;
+	   private String firstName;
+	   private String lastName;
+	   private Date dateOfBirth;
+	   private long phoneNo;
+	   private String email;
+	   
+	   
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
