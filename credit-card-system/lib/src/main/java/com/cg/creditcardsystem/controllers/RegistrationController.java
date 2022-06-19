4244845d401407ec2b9e336c87e452170f81321e
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2022-06-19 14:52:22 +0530

    push

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/controllers/RegistrationController.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/controllers/RegistrationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.creditcardsystem.entities.Registration;
 import com.cg.creditcardsystem.service.RegistrationServiceImpl;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/register")
 public class RegistrationController {
