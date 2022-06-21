c9d6b60021c7b4e0506a5c963684f0453dc0977d
saurabhsase <saurabh.sase@gmail.com>
2022-06-21 15:57:58 +0530

    update

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/controllers/AddressController.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/controllers/AddressController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cg.creditcardsystem.dto.AddressDto;
 import com.cg.creditcardsystem.entities.Address;
 import com.cg.creditcardsystem.service.AddressServiceImpl;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/address")
 public class AddressController {
