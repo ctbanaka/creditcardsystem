e8607bfcc32a3c9dbc0424ea73a82018f3fc8524
Babukalluru <kalluru.babu@capgemini.com>
2022-06-24 07:24:57 +0530

    u

--- credit-card-system/lib/src/main/java/com/cg/creditcardsystem/entities/CreditCard.java
+++ credit-card-system/lib/src/main/java/com/cg/creditcardsystem/entities/CreditCard.java
@@ -13,6 +13,7 @@ import javax.persistence.Table;
 public class CreditCard {
 	
 	@Id
+	
 	@Column(name="card_number",length=16, unique=true)
 	private long cardNo;
 	@Column(name="cardtype",length=20,nullable=false)
